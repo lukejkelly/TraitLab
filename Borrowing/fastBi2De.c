2996024b7d53319e318023ca4ca0a70e9f57e079
Luke Kelly <kelly@stats.ox.ac.uk>
2017-09-24 15:46:08 +0100

    Updating C functions to be consistent with Matlab

--- Borrowing/fastBi2De.c
+++ Borrowing/fastBi2De.c
@@ -1,9 +1,9 @@
 /*
- * fastBi2De_c_par.c - used to convert binary arrays to decimal vectors.
+ * fastBi2De.c - convert binary arrays to decimal vectors.
  *
  * The calling syntax is:
  *
- *		P_d = fastBi2De_c_par(P_b),
+ *		P_d = fastBi2De(P_b, 'left-msb'),
  *
  * where
  *	- P_b is a double array whose rows correspond to binary vectors with
@@ -13,34 +13,34 @@
  *      length M * L, hence the awkward indexing used below.
  *  - P_d is a double vector whose entries are the decimal representations
  *      of the rows in P_b.
- * 
+ *  - 'left-msb' is ignored.
+ *
+ * Uncomment the //-commented lines to include the parallel version.
+ *
  * This is a MEX-file for MATLAB.
-*/
+ */
 
 /* Header files. */
 #include "mex.h"
 #include <omp.h>
 
 /*
- * Computational routine.
-*/
+ * Computational routines.
+ */
 
 /* Parallelised binary-to-decimal function. */
 void fastBi2De_par(double *P_b, int M, int L, double *P_d)
 {
-    /* Loop indices. */
-    int i, j;
-
     /* Dynamic threading. */
     omp_set_dynamic(1);
 
-	#pragma omp parallel for shared(P_b, M, L, P_d) private(i, j)
-    for(i = 0; i < M; i++) /* Looping over rows of P_b/P_d. */
+	  #pragma omp parallel for shared(P_b, M, L, P_d) private(i, j)
+    for(int i = 0; i < M; i++) /* Looping over rows of P_b/P_d. */
     {
         /* Looping over columns of P_b(:, j) to populate P_d(j). */
-        for(j = 0; j < L; j++)
+        for(int j = 0; j < L; j++)
         {
-            /* We use bit-shifting to convert binary to decimal. */
+            /* Use bit-shifting to convert binary to decimal. */
             P_d[i] += ((double) ( ((int) P_b[i + j * M]) << (L - 1 - j) ) );
         }
     }
@@ -49,15 +49,12 @@ void fastBi2De_par(double *P_b, int M, int L, double *P_d)
 /* Serial binary-to-decimal function. */
 void fastBi2De_ser(double *P_b, int M, int L, double *P_d)
 {
-    /* Loop indices. */
-    int i, j;
-
-    for(i = 0; i < M; i++) /* Looping over rows of P_b/P_d. */
+    for(int i = 0; i < M; i++) /* Looping over rows of P_b/P_d. */
     {
         /* Looping over columns of P_b(:, j) to populate P_d(j). */
-        for(j = 0; j < L; j++)
+        for(int j = 0; j < L; j++)
         {
-            /* We use bit-shifting to convert binary to decimal. */
+            /* Use bit-shifting to convert binary to decimal. */
             P_d[i] += ((double) ( ((int) P_b[i + j * M]) << (L - 1 - j) ) );
         }
     }
@@ -66,16 +63,15 @@ void fastBi2De_ser(double *P_b, int M, int L, double *P_d)
 /*
  * The gateway function.
 */
-void mexFunction(int nlhs, mxArray *plhs[], int nrhs,
-    const mxArray *prhs[])
+void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
 {
     /* Checking number of arguments. */
-    
+
     /* Checking correct number of input arguments. */
-    if(nrhs != 1)
+    if(nrhs != 2)
     {
         mexErrMsgIdAndTxt("MyToolbox:arrayProduct:nrhs",
-            "One input required.");
+            "Two inputs required.");
     }
 
     /* Checking correct number of output arguments. */
@@ -85,14 +81,14 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs,
             "One output required.");
     }
 
-	/* Checking input types. */
-    
+  	/* Checking input types. */
+
     /* P_b is an M x L binary array, where M is often 2^L - 1. */
-	if( !mxIsDouble(prhs[0]) || mxIsComplex(prhs[0]))
-	{
-		mexErrMsgIdAndTxt("MyToolbox:arrayProduct:notDouble",
-	        "P_b must be type double.");
-	}
+  	if( !mxIsDouble(prhs[0]) || mxIsComplex(prhs[0]))
+  	{
+  		mexErrMsgIdAndTxt("MyToolbox:arrayProduct:notDouble",
+  	        "P_b must be type double.");
+  	}
 
     /* Declaring input variable. */
     double *P_b;    /* Binary array. */
@@ -103,20 +99,20 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs,
     /* Declaring associated variables. */
     int M;      /* Scalar number of rows in P_b. */
     int L;      /* Scalar number of columns in P_b. */
-        
+
     /* Getting pointers to input data. */
     P_b = mxGetPr(prhs[0]);
 
     /* Dimensions of P_b. */
     M = mxGetM(prhs[0]);
     L = mxGetN(prhs[0]);
-    
+
     /* Preparing output data.*/
     plhs[0] = mxCreateDoubleMatrix(M, 1, mxREAL);
-    
+
     /* Assigning a pointer to output vector.*/
     P_d = mxGetPr(plhs[0]);
-    
+
     /* Calling the computational routine.*/
     if(L <= 30)
     {
--- Borrowing/fastDe2Bi.c
+++ Borrowing/fastDe2Bi.c
@@ -1,53 +1,51 @@
 /*
- * fastDe2Bi_c_par.c - used to convert decimal vectors to binary arrays.
+ * fastDe2Bi_c_par.c - convert decimal vectors to binary arrays.
  * Adapted from a similar function by Giovanni Motta.
  *
  * The calling syntax is:
  *
- *		P_b = fastDe2Bi_c_par(P_d, L),
+ *		P_b = fastDe2Bi(P_d, L, 'left-msb'),
  *
  * where
  *	- P_b is a double array whose rows correspond to binary vectors with
  *      the most significant bit on the left-hand side ('left-msb' in
- *      bi2de function). We note that P_b has M rows and L columns when
+ *      de2bi function). We note that P_b has M rows and L columns when
  *      passed to C. However, in C it is stacked by columns as a vector of
  *      length M * L, hence the awkward indexing used below.
  *  - P_d is a double vector whose entries are the decimal representations
  *      of the rows in P_b.
  *  - L is the length of each binary vector / row of P_b.
- * 
+ *  - 'left-msb' is ignored.
+ *
+ * Uncomment the //-commented lines to include the parallel version.
+ *
  * This is a MEX-file for MATLAB.
-*/
+ */
 
 /* Header files. */
 #include "mex.h"
 #include <omp.h>
 
 /*
- * Computational routine.
-*/
+ * Computational routines.
+ */
 
 /* Parallelised binary-to-decimal function. */
 void fastDe2Bi_par(double *P_b, int M, double *L, double *P_d)
 {
-    /* Loop indices. */
-    int i, j;
-
     /* Dynamic threading. */
     omp_set_dynamic(1);
 
-	#pragma omp parallel for shared(P_b, M, L, P_d) private(i, j)
-    for(i = 0; i < M; i++) /* Looping over rows of P_b/P_d. */
+	  #pragma omp parallel for shared(P_b, M, L, P_d) private(i, j)
+    for(int i = 0; i < M; i++) /* Looping over rows of P_b/P_d. */
     {
         /* Storing P_d[i] as an int so that we can bitshift it. */
-        int k;
-        k = (int) P_d[i];
-        
+        int k = (int) P_d[i];
+
         /* Looping over columns of P_b(i, :) to populate it. */
-        for(j = 0; j < ((int) *L); j++)
+        for(int j = 0; j < ((int) *L); j++)
         {
-            /* We use bit-shifting and bitwise AND to convert decimal to
-             * binary. */
+            /* Use bit-shifting and bitwise AND to convert decimal to binary. */
             P_b[i + (((int) *L) - j - 1) * M] = ((double) ((k >> j) & 0x1));
         }
     }
@@ -56,20 +54,15 @@ void fastDe2Bi_par(double *P_b, int M, double *L, double *P_d)
 /* Serial binary-to-decimal function. */
 void fastDe2Bi_ser(double *P_b, int M, double *L, double *P_d)
 {
-    /* Loop indices. */
-    int i, j;
-
-    for(i = 0; i < M; i++) /* Looping over rows of P_b/P_d. */
+    for(int i = 0; i < M; i++) /* Looping over rows of P_b/P_d. */
     {
         /* Storing P_d[i] as an int so that we can bitshift it. */
-        int k;
-        k = (int) P_d[i];
-        
+        int k = (int) P_d[i];
+
         /* Looping over columns of P_b(i, :) to populate it. */
-        for(j = 0; j < ((int) *L); j++)
+        for(int j = 0; j < ((int) *L); j++)
         {
-            /* We use bit-shifting and bitwise AND to convert decimal to
-             * binary. */
+            /* Use bit-shifting and bitwise AND to convert decimal to binary. */
             P_b[i + (((int) *L) - j - 1) * M] = ((double) ((k >> j) & 0x1));
         }
     }
@@ -78,16 +71,15 @@ void fastDe2Bi_ser(double *P_b, int M, double *L, double *P_d)
 /*
  * The gateway function.
 */
-void mexFunction(int nlhs, mxArray *plhs[], int nrhs,
-    const mxArray *prhs[])
+void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
 {
     /* Checking number of arguments. */
-    
+
     /* Checking correct number of input arguments. */
-    if(nrhs != 2)
+    if(nrhs != 3)
     {
         mexErrMsgIdAndTxt("MyToolbox:arrayProduct:nrhs",
-            "Two inputs required: P_d and L.");
+            "Three inputs required: P_d,  L and 'left-msb'.");
     }
 
     /* Checking correct number of output arguments. */
@@ -97,21 +89,21 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs,
             "One output required: P_b.");
     }
 
-	/* Checking input types. */
-    
+	  /* Checking input types. */
+
     /* P_d is an M x 1 decimal array, where M is often 2^L - 1. */
-	if( !mxIsDouble(prhs[0]) || mxIsComplex(prhs[0]))
-	{
-		mexErrMsgIdAndTxt("MyToolbox:arrayProduct:notDouble",
-	        "P_d must be type double.");
-	}
-    
-	if( !mxIsDouble(prhs[1]) || mxIsComplex(prhs[1])
-        || mxGetNumberOfElements(prhs[1]) != 1 )
-	{
-		mexErrMsgIdAndTxt("MyToolbox:arrayProduct:notScalar",
-	        "L must be a scalar of type double.");
-	}    
+  	if( !mxIsDouble(prhs[0]) || mxIsComplex(prhs[0]))
+  	{
+  		mexErrMsgIdAndTxt("MyToolbox:arrayProduct:notDouble",
+  	        "P_d must be type double.");
+  	}
+
+  	if( !mxIsDouble(prhs[1]) || mxIsComplex(prhs[1])
+          || mxGetNumberOfElements(prhs[1]) != 1 )
+  	{
+  		mexErrMsgIdAndTxt("MyToolbox:arrayProduct:notScalar",
+  	        "L must be a scalar of type double.");
+  	}
 
     /* Declaring input variables. */
     double *P_d;    /* Vector of integers. */
@@ -122,7 +114,7 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs,
 
     /* Declaring associated variable. */
     int m, n, M;      /* Scalar number of rows in P_b. */
-        
+
     /* Getting pointers to input data. */
     P_d = mxGetPr(prhs[0]);
     L = mxGetPr(prhs[1]);
@@ -130,15 +122,15 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs,
     /* Number of rows in P_b. */
     m = mxGetM(prhs[0]);
     n = mxGetN(prhs[0]);
-    
+
     M = (m > n) ? m : n;
-    
+
     /* Preparing output data.*/
     plhs[0] = mxCreateDoubleMatrix(M, *L, mxREAL);
-    
+
     /* Assigning a pointer to output array.*/
     P_b = mxGetPr(plhs[0]);
-    
+
     /* Calling the computational routine.*/
     if(*L <= 12)
     {
