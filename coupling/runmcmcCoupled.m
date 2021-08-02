748ef689c3c3651557ac8b4a94a6f823a017be44
Luke Kelly <kelly.luke.joseph@gmail.com>
2021-08-02 20:00:34 +0200

    Marginal sampler after coupling (#51)

--- coupling/runmcmcCoupled.m
+++ coupling/runmcmcCoupled.m
@@ -10,9 +10,9 @@ start = 1;
 % write details of run in par file
 parfilename = [fsu.OUTPATH, fsu.OUTFILE, '.par'];
 fprintf('Writing parameter file %s\n\n', parfilename);
-wroteparfile = writerunparfile(parfilename,fsu,fsu.MCMCINITTREENUM);
+wroteparfile = writerunparfile(parfilename, fsu, fsu.MCMCINITTREENUM);
 if ~wroteparfile
-    fprintf('Encountered problem writing parameter file %s.\nEverything else seems ok.\n',parfilename);
+    fprintf('Encountered problem writing parameter file %s.\n', parfilename);
 end
 
 [state_x, state_y] = deal(state);
@@ -39,8 +39,8 @@ for t = start:lag_subsample
     ignoreearlywarn = (t <= 3);
     [state_x, pa_x] = Markov(mcmc, model, state_x, ignoreearlywarn);
     btime = cputime - atime;
-    handles_x = runmcmcCoupled.writeMcmcOutputs(handles_x, state_x, btime, ...
-                                                fsu, t, pa_x, model, data);
+    handles_x = runmcmcCoupled.writeMcmcOutputs(...
+        handles_x, state_x, btime, fsu, t, pa_x, model, data);
 end
 
 % Housekeeping nodes in y to match x
@@ -51,30 +51,43 @@ runmcmcCoupled.writeProportionAccepted(zeros(mcmc.update.Nmvs, 1), ...
                                        handles_x.output.path, ...
                                        [handles_x.output.file, 'y']);
 
-% Run chain until x reaches finish iterations or coupling, whichever is largest
-while t < finish || ~checkCoupling(state_x, state_y)
+% Run until coupling or x reaches finish iterations, whichever is largest
+isCoupled = checkCoupling(state_x, state_y);
+if isCoupled
+    error('Initial states should not be coupled');
+end
+
+while t < finish || ~isCoupled
     t = t + 1;
 
     runmcmcCoupled.borrowingCheck(state_x);
     runmcmcCoupled.borrowingCheck(state_y);
 
-    %update the Markov chain (mcmc.subsample) steps
-    atime=cputime;
-    ignoreearlywarn= (t<=3); % Ignore warnings which are not alarming when they occur early in the chain. RJR 12/06/11.
-    [state_x, state_y, pa_x, pa_y, pa_xy] ...
-        = MarkovCoupled(mcmc, model, state_x, state_y, ignoreearlywarn);
-    btime=cputime-atime;
+    atime = cputime;
+    ignoreearlywarn = (t <= 3);
+    if isCoupled
+        [state_x, pa_x] = Markov(mcmc, model, state_x, ignoreearlywarn);
+        state_y = state_x;
+        pa_y = pa_x;
+        pa_xy = pa_x;
+    else
+        [state_x, state_y, pa_x, pa_y, pa_xy, isCoupled] = MarkovCoupled(...
+            mcmc, model, state_x, state_y, ignoreearlywarn);
+        if isCoupled
+            runmcmcCoupled.writeCouplingTime(handles_x, t);
+        end
+    end
+    btime = cputime - atime;
 
     % Write outputs and update handles
-    handles_x = runmcmcCoupled.writeMcmcOutputs(handles_x, state_x, btime, ...
-                                                fsu, t, pa_x, model, data);
-    handles_y = runmcmcCoupled.writeMcmcOutputs(handles_y, state_y, btime, ...
-                                                fsu, t - lag_subsample, ...
-                                                pa_y, model, data);
+    handles_x = runmcmcCoupled.writeMcmcOutputs(...
+        handles_x, state_x, btime, fsu, t, pa_x, model, data);
+    handles_y = runmcmcCoupled.writeMcmcOutputs(...
+        handles_y, state_y, btime, fsu, t - lag_subsample, pa_y, model, data);
 
     % Write proportion of moves accepted
-    runmcmcCoupled.writeProportionAccepted(pa_xy, handles_x.output.path, ...
-                                           [handles_x.output.file, 'y'], t);
+    runmcmcCoupled.writeProportionAccepted(...
+        pa_xy, handles_x.output.path, [handles_x.output.file, 'y'], t);
 end
 
 disp(['MCMC run finished at ' datestr(clock)]);
