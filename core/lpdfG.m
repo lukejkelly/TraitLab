b6d1f5b76e97885f66c218b1a7caf022522bb8dc
Luke Kelly <kelly.luke.joseph@gmail.com>
2023-06-30 18:14:05 +0100

    Fix default check

--- core/lpdfG.m
+++ core/lpdfG.m
@@ -1,7 +1,7 @@
 function lp = lpdfG(x, a, b)
 % Evaluate log PDF at point x of Gamma with shape a and rate b
 
-if any([x, a, b] <= 0)
+if x < 0 || any([a, b] <= 0)
     error('arguments must be non-negative: x = %g, a = %g, b = %g', x, a, b);
 end
 
